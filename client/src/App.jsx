756d97d487bf2aeffc48cd9173107e370dfb11c6
neillycubed <neillybusiness3@gmail.com>
2021-03-20 21:04:47 -0700

    add logo and title to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 import styles from './App.module.css'
 
+import logo from './imgs/logo.png'
+
 const App = () => (
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+        <img className={styles.image} src={logo} alt='Covid Maps' />
+        <h1>COVID MAPS</h1>
+    </div>
 )
 
 export default App
