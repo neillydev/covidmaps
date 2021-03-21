bcd236ca69044a4cb2521bb7e7a3337eb722e85b
neillycubed <neillybusiness3@gmail.com>
2021-03-20 21:09:30 -0700

    create CountryList component

---
+++ client/src/components/CountryList/CountryList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './CountryList.module.css'
+
+const CountryList = () => {
+
+    //get country list passed in from api data
+
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
+
+export default CountryList
