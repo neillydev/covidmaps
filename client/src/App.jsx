d3cce49fbec2d25952fae48d04dfaa2485890f84
neillycubed <neillybusiness3@gmail.com>
2021-03-20 21:09:40 -0700

    add CountryList component to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import CountryList from './components/CountryList/CountryList'
 
 import styles from './App.module.css'
-
 import logo from './imgs/logo.png'
+
+
 
 const App = () => (
     <div className={styles.container}>
         <img className={styles.image} src={logo} alt='Covid Maps' />
         <h1>COVID MAPS</h1>
+        <CountryList />
     </div>
 )
 
