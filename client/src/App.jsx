e531c9834ec91261ad4368c3be08354b0caed129
neillycubed <neillybusiness3@gmail.com>
2021-03-26 09:53:45 -0700

    style App page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import CountryList from './components/CountryList/CountryList'
 
 import styles from './App.module.css'
+
+import CountryList from './components/CountryList/CountryList'
+import Card from './components/Card/Card'
+import Map from './components/Map/Map'
+
 import logo from './imgs/logo.png'
-
-
 
 const App = () => (
     <div className={styles.container}>
-        <img className={styles.image} src={logo} alt='Covid Maps' />
-        <h1>COVID MAPS</h1>
-        <CountryList />
+        <div className={styles.header}>
+            <div className={styles['header-corner']}></div>
+            <img className={`${styles.title} ${styles.image}`} src={logo} alt='Covid Maps' />
+            <h1 className={styles.title}>COVID MAPS</h1>
+        </div>
+        <div className={styles['content-container']}>
+            <CountryList />
+            <Map />
+        </div>
     </div>
 )
 
