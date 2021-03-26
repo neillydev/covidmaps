e5901cbe91294ef770bdf87bc16629a11113dc21
neillycubed <neillybusiness3@gmail.com>
2021-03-26 03:39:22 -0700

    add input element and icon to SearchBar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -6,7 +6,8 @@ const SearchBar = () => {
     return (
         <div className={styles['search-container']}>
             <div className={styles['search-bar']}>
-                
+                <i class="fas fa-search fa2x"></i>
+                <input className={styles['search-input']} placeholder="Search location" />
             </div>
         </div>
     )
