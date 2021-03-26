0855e269a31fc81892e872b08c21f5eca8209ea5
neillycubed <neillybusiness3@gmail.com>
2021-03-26 09:43:56 -0700

    create Map component for geographic visualization

---
+++ client/src/components/Map/Map.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from './Map.module.css'
+
+
+const Map = () => {
+
+    return (
+        <div className={styles['map-container']}>
+            <h3 className={styles['map-title']}>Global Map</h3>
+            <div className={styles['map-body']}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Map
