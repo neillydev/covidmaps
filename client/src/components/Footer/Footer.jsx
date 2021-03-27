29a2af0a854e1e59371fc7b117dc319a33852956
neillycubed <neillybusiness3@gmail.com>
2021-03-27 03:26:41 -0700

    refactor data flow and add CountUp animation

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,26 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+import CountUp from 'react-countup'
 
 import styles from './Footer.module.css'
 
-import { fetchCountries } from '../../api'
-
-const Footer = () => {
+const Footer = ({ confirmed, recovered, deaths }) => {
     return (
         <div className={styles['footer-container']}>
             <div className={styles['footer-body']}>
                 <div className={styles['footer-subbody']}>
                     <h4 className={styles['footer-subtitle']}>Cases</h4>
-                    <h2 className={styles['footer-subtext']}>0</h2>
+                    <CountUp start={0} end={confirmed} duration={2.5} separator=',' />
                 </div>
                 <div className={styles['footer-separator']} />
                 <div className={styles['footer-subbody']}>
                     <h4 className={styles['footer-subtitle']}>Recovered</h4>
-                    <h2 className={styles['footer-subtext']}>0</h2>
+                    <CountUp start={0} end={recovered} duration={2.5} separator=',' />
                 </div>
                 <div className={styles['footer-separator']} />
                 <div className={styles['footer-subbody']}>
                     <h4 className={styles['footer-subtitle']}>Deaths</h4>
-                    <h2 className={styles['footer-subtext']}>0</h2>
+                    <CountUp start={0} end={deaths} duration={2.5} separator=',' />
                 </div>
             </div>
         </div>
