e7d0f22a3de5efdd52c9b519fb68623f33c0d189
neillycubed <neillybusiness3@gmail.com>
2021-03-27 03:27:33 -0700

    fix fetchCountryData response handling

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,10 +4,21 @@ const url = 'https://covid-api.mmediagroup.fr/v1/cases';
 
 export const fetchCountries = async () => {
     try {
-        const { data: allData } = await axios.get(url)
-        let countryArray = [];
-        for(const [country] of Object.entries(allData)) countryArray.push(country);
+        const { data: allData } = await axios.get(url);
+        let countryArray = ["Global", "US"];
+        for(const [country] of Object.entries(allData)) {
+            countryArray.push(countryArray.includes(country) ? null : country);
+        }
         return countryArray;
+    }
+    catch(error) {
+
+    }
+}
+
+export const fetchCountryData = async (country) => {
+    try {
+        return await axios.get(`${url}?country=${country}`);
     }
     catch(error) {
 
