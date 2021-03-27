e738f3428dbd53aab84124bbd0df0c2a9af9a41f
neillycubed <neillybusiness3@gmail.com>
2021-03-27 03:28:39 -0700

    refactor data handling with state and hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import styles from './App.module.css'
 
 import CountryList from './components/CountryList/CountryList'
-import Card from './components/Card/Card'
+import Footer from './components/Footer/Footer'
 import Map from './components/Map/Map'
 
 import logo from './imgs/logo.png'
 
-const App = () => (
-    <div className={styles.container}>
-        <div className={styles.header}>
-            <div className={styles['header-corner']}></div>
-            <img className={`${styles.title} ${styles.image}`} src={logo} alt='Covid Maps' />
-            <h1 className={styles.title}>COVID MAPS</h1>
+import { fetchCountries, fetchCountryData } from './api'
+
+const App = () => {
+    const [country, setCountry] = useState('');
+    const [countries, setCountries] = useState([]);
+    const [cases, setCases] = useState(0);
+    const [recoveries, setRecoveries] = useState(0);
+    const [deathToll, setDeathToll] = useState(0);
+
+    const handleClick = (e) => {
+        setCountry(e.target.innerText);
+    }
+    
+    useEffect(() => {
+        setCountry('Global');
+    }, []);
+
+    useEffect(() => {
+        const getCountries = async () => {
+            setCountries(await fetchCountries());
+        };
+
+        getCountries();
+    }, [countries]);
+
+    useEffect(() => {
+        const getCaseData = async () => {
+            await fetchCountryData(country).then(({data: { All: {confirmed, recovered, deaths} }}) => {
+                setCases(confirmed);
+                setRecoveries(recovered);
+                setDeathToll(deaths);
+            });
+        };
+
+        getCaseData();
+    }, [country])
+
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles['header-corner']}></div>
+                <img className={`${styles.title} ${styles.image}`} src={logo} alt='Covid Maps' />
+                <h1 className={styles.title}>COVID MAPS</h1>
+            </div>
+            <div className={styles['content-container']}>
+                <CountryList countries={countries} country={country} handleClick={handleClick} />
+                <Map />
+            </div>
+            <Footer confirmed={cases} recovered={recoveries} deaths={deathToll} />
         </div>
-        <div className={styles['content-container']}>
-            <CountryList />
-            <Map />
-        </div>
-        <Footer />
-    </div>
-)
+    )
+}
 
 export default App
