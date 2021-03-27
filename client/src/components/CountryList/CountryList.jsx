bee7a2ff40954740b1b1381f53322f86f6ff91e2
neillycubed <neillybusiness3@gmail.com>
2021-03-27 03:26:06 -0700

    refactor data flow with props

--- client/src/components/CountryList/CountryList.jsx
+++ client/src/components/CountryList/CountryList.jsx
@@ -1,28 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 import styles from './CountryList.module.css'
 
 import CountryCard from '../CountryCard/CountryCard'
 import SearchBar from '../SearchBar/SearchBar'
 
-import { fetchCountries } from '../../api'
-
-const CountryList = () => {
-    const [countries, setCountries] = useState(['Worldwide']);
-
-    useEffect(() => {
-        const getCountries = async () => {
-            setCountries(await fetchCountries());
-        };
-
-        getCountries();
-    }, [countries]);
-
+const CountryList = ({ countries, country, handleClick  }) => {
     return (
         <div className={styles['list-container']}>
             <SearchBar />
             <div className={styles['list-body']}>
-                <CountryCard countryList={countries} />
+                <CountryCard countries={countries} country={country} handleClick={handleClick} />
             </div>
         </div>
     )
