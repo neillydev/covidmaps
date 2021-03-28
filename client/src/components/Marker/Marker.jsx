8e8e6707b27aed0fd35583a721f2cbc7f71a91b6
neillycubed <neillybusiness3@gmail.com>
2021-03-28 16:21:51 -0700

    create Marker component with styling

---
+++ client/src/components/Marker/Marker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './Marker.module.css'
+
+const Marker = () => {
+    return (
+        <div className={styles['marker-container']} />
+    )
+}
+
+export default Marker
