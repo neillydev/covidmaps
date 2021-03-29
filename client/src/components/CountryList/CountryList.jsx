53c13faaf8893379a977227ff9c2a1cfd2922d52
neillycubed <neillybusiness3@gmail.com>
2021-03-29 08:56:55 -0700

    add country filtering to SearchBar

--- client/src/components/CountryList/CountryList.jsx
+++ client/src/components/CountryList/CountryList.jsx
@@ -5,10 +5,10 @@ import styles from './CountryList.module.css'
 import CountryCard from '../CountryCard/CountryCard'
 import SearchBar from '../SearchBar/SearchBar'
 
-const CountryList = ({ countries, country, handleClick  }) => {
+const CountryList = ({ countries, country, handleClick, handleKeyDown  }) => {
     return (
         <div className={styles['list-container']}>
-            <SearchBar />
+            <SearchBar handleKeyDown={handleKeyDown} />
             <div className={styles['list-body']}>
                 <CountryCard countries={countries} country={country} handleClick={handleClick} />
             </div>
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 import styles from './SearchBar.module.css'
 
-const SearchBar = () => {
+const SearchBar = ({ handleKeyDown }) => {
     return (
         <div className={styles['search-container']}>
             <div className={styles['search-bar']}>
-                <i class="fas fa-search fa2x"></i>
-                <input className={styles['search-input']} placeholder="Search location" />
+                <i className="fas fa-search fa2x"></i>
+                <input className={styles['search-input']} placeholder="Search location" onKeyDown={handleKeyDown} />
             </div>
         </div>
     )
