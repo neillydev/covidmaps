e312694aa57361dba59ec83ae0ff14f5360a36de
neillycubed <neillybusiness3@gmail.com>
2021-03-29 09:16:24 -0700

    add dynamic icon sizing based on case amount

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react';
 import Marker from '../Marker/Marker'
+import Coronavirus from '../Icons/Coronavirus'
 
 import styles from './Map.module.css'
 
 const Map = ({center, zoom, onMouseEnter, coordinates}) => {
+
+    const sizes = {
+        '20': 20,
+        '1000': 24,
+        '10000': 28,
+        '100000': 30,
+        '500000': 34,
+        '750000': 38,
+        '1000000': 40,
+        '2000000': 44,
+        '3000000': 48,
+    }
 
     return (
         <div className={styles['map-container']}>
@@ -19,7 +32,8 @@ const Map = ({center, zoom, onMouseEnter, coordinates}) => {
                     yesIWantToUseGoogleMapApiInternals
                 >
                     {coordinates.map(coordinateObj => {
-                        return ((coordinateObj.lat && coordinateObj.long) != undefined  ? <Marker lat={coordinateObj.lat} lng={coordinateObj.long} /> : null)
+                        let size = sizes[Object.keys(sizes).filter((sizeValue, i) => (sizeValue <= coordinateObj.cases && Object.keys(sizes)[i+1] > coordinateObj.cases ))];
+                        return ((coordinateObj.lat && coordinateObj.long) !== undefined  ? <Coronavirus lat={coordinateObj.lat} lng={coordinateObj.long} width={size} height={size} /> : null)
                     })}
                 </GoogleMapReact>
       </div>
