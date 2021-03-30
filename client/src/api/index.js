dc5deb1faf10b6bcbfc88d91a49aaa4ce2f2c596
neillycubed <neillybusiness3@gmail.com>
2021-03-30 10:09:08 -0700

    refactor API data parsing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,7 +4,16 @@ const url = 'https://covid-api.mmediagroup.fr/v1/cases';
 
 export const fetchCountries = async () => {
     try {
-        return await axios.get(url);
+        return await axios.get(url).then((response)=>{
+            const { data: allData } = response;
+            let countryArray = ["Global", "US"];
+            for(const [country] of Object.entries(allData)) {
+                countryArray.push(countryArray.includes(country) ? null : country);
+            }
+            return countryArray;
+        }).catch(error => {
+
+        });
     }
     catch(error) {
 
@@ -13,7 +22,11 @@ export const fetchCountries = async () => {
 
 export const fetchCountryData = async (country) => {
     try {
-        return await axios.get(`${url}?country=${country}`);
+        return await axios.get(`${url}?country=${country}`).then((response)=>{
+            return response;
+        }).catch(error => {
+
+        });
     }
     catch(error) {
 
@@ -22,7 +35,21 @@ export const fetchCountryData = async (country) => {
 
 export const fetchCoordinates = async () => {
     try {
-        return await axios.get(`${url}`);
+        return await axios.get(url).then((response)=>{
+            const { data: allData } = response;
+            let coordinateArray = [];
+            for(const [country, {All: countryData}] of Object.entries(allData)) {
+                coordinateArray.push({
+                    lat: countryData.lat,
+                    long: countryData.long,
+                    cases: countryData.confirmed,
+                    country: countryData.country
+                });
+            }
+            return (coordinateArray);
+        }).catch(error => {
+
+        });
     }
     catch(error) {
 
