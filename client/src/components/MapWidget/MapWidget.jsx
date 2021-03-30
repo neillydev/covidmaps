7e4eb2b637f606bac46ad84fb5b0dba55f211cfc
neillycubed <neillybusiness3@gmail.com>
2021-03-30 09:01:38 -0700

    create MapWidget to display data on hover

---
+++ client/src/components/MapWidget/MapWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './MapWidget.module.css';
+
+const MapWidget = () => {
+    return (
+        <div className={styles['widget-container']}>
+            
+        </div>
+    )
+}
+
+export default MapWidget
