b0d46d1bee2806ad5263adf864cfe63df09b7c81
neillycubed <neillybusiness3@gmail.com>
2021-03-30 10:26:32 -0700

    add MapWidget case data headers

--- client/src/components/MapWidget/MapWidget.jsx
+++ client/src/components/MapWidget/MapWidget.jsx
@@ -6,6 +6,9 @@ const MapWidget = ({ country }) => {
     return (
         <div className={styles['widget-container']}>
             <h2 className={styles['widget-header']}>{country}</h2>
+            <h3 className={styles['widget-text']}>Cases:</h3>
+            <h3 className={styles['widget-text']}>Recoveries:</h3>
+            <h3 className={styles['widget-text']}>Deaths:</h3>
         </div>
     )
 }
