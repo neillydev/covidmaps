8226e6ff34c5674cc0e065402e81165e875c34b3
neillycubed <neillybusiness3@gmail.com>
2021-04-03 18:43:01 -0700

    refactor div organization in App header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
-import CountryList from './components/CountryList/CountryList'
+import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import CountryList from './components/CountryList/CountryList'
 import Map from './components/Map/Map'
 
 import styles from './App.module.css'
@@ -20,9 +21,14 @@ const App = () => {
     return (
         <div className={styles.container}>
             <div className={styles.header}>
-                <div className={styles['header-corner']}></div>
-                <img className={`${styles.title} ${styles.image}`} src={logo} alt='Covid Maps' />
-                <h1 className={styles.title}>COVID MAPS</h1>
+                <div className={styles.first}>
+                    <div className={styles['header-corner']}></div>
+                    <img className={`${styles.title} ${styles.image}`} src={logo} alt='Covid Maps' />
+                    <h1 className={styles.title}>COVID MAPS</h1>
+                </div>
+                <div className={styles.second}>
+                    <NavBar />
+                </div>
             </div>
             <div className={styles['content-container']}>
                 <CountryList country={country} handleClick={handleClick} />
