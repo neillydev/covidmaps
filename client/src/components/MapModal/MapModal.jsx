0b253ce07cd366f43c0ff68342f1b1ed002b3967
neillycubed <neillybusiness3@gmail.com>
2021-04-05 01:13:02 -0700

    add country header to map modals

--- client/src/components/MapModal/MapModal.jsx
+++ client/src/components/MapModal/MapModal.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import styles from './MapModal.module.css';
 
-const MapModal = () => {
+const MapModal = ({ country }) => {
     return (
         <div className={styles['modal-container']}>
             <div className={styles['modal-anchor']}>
                 <div className={styles['modal-body']}>
-
+                    <h2 className={styles['modal-header']}>{country}</h2>
                 </div>
             </div>
         </div>
