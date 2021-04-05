728a150e1dfcdf8816ce266ad79004d31705542d
neillycubed <neillybusiness3@gmail.com>
2021-04-05 00:57:45 -0700

    add MapModal component for icon popup

---
+++ client/src/components/MapModal/MapModal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './MapModal.module.css';
+
+const MapModal = () => {
+    return (
+        <div className={styles['modal-container']}>
+            <div className={styles['modal-anchor']}>
+                <div className={styles['modal-body']}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MapModal
